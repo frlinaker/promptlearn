ab220220dddc9668edc76c5f880a1413311ef1d8
Fredrik Linaker <fredrik.linaker@gmail.com>
2025-06-28 00:43:02 -0700

    generate fresh samples from heuristic

---
+++ examples/generate_samples.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+Example: Using PromptRegressor to generate samples
+"""
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from promptlearn import PromptRegressor
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# Create training data: y = 2x + 3
+X = np.array([[-1], [0], [1], [2], [3]])
+y = np.array([1, 3, 5, 7, 9])  # 2x + 3
+
+# Fit the prompt-based regressor
+model = PromptRegressor(verbose=True)
+model.fit(X, y)
+
+samples = model.sample(10)
+print(samples)
